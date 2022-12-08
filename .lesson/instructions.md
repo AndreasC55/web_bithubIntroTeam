8ba3fe32137a43bcb9fd7264aa05dd4136172a62
ANDREAS CIURILA <119446650+AndreasC55@users.noreply.github.com>
2022-12-08 18:57:40 +0000

    Form.php

--- .lesson/instructions.md
+++
@@ -1,2 +0,0 @@
-# Instructions  
-https://docs.google.com/document/d/12t9pwG2SMyLE_sSud1A6Jfz6qlw7XXgg7H9ygb404Uk/edit
---
+++ redirect.php
@@ -0,0 +1,5 @@
+$firstName = $_POST['first-name'];
+$lastName = $_POST['last-name'];
+$email = $_POST['email'];
+$phoneNumber = $_POST['phone-number'];
+$relationshipStatus = $_POST['relationship-status'];
