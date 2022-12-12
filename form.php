37effa2edf6e40cdafa4e0b0a68eca30ea21e6d3
ANDREAS CIURILA <119446650+AndreasC55@users.noreply.github.com>
2022-12-12 18:48:24 +0000

    forem

---
+++ form.php
@@ -0,0 +1,21 @@
+<form action="/redirectForm.php">
+   <label for="first-name">First Name:</label><br>
+   <input type="text" id="first-name" name="first-name"><br>
+
+   <label for="last-name">Last Name:</label><br>
+   <input type="text" id="last-name" name="last-name"><br>
+
+   <label for="email">Email:</label><br>
+   <input type="email" id="email" name="email"><br>
+
+   <label for="phone-number">Phone Number:</label><br>
+   <input type="tel" id="phone-number" name="phone-number"><br>
+
+   <label for="relationship-status">Relationship Status:</label><br>
+   <select id="relationship-status" name="relationship-status">
+      <option value="single">Friend</option>
+      <option value="dating">Family</option>
+      <option value="engaged">Coworker</option>
+   </select><br>
+<input type="submit" value="Submit">
+</form>
