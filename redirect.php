fc7dd4b3271734905d317427cbdafb868c3dca86
Jayden Black <119446626+jaybobo0@users.noreply.github.com>
2022-12-13 19:10:17 +0000

    added a header location to redirect.php

--- redirect.php
+++ redirect.php
@@ -4,5 +4,7 @@ $lastName = $_POST['last-name'];
 $email = $_POST['email'];
 $phoneNumber = $_POST['phone-number'];
 $relationshipStatus = $_POST['relationship-status'];
+
+header("Location: /form.php?fName=$fName&lName=$lName&reason=$reason&age=$age&state=$state&comment=$comment");
 ?>
 
