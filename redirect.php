975d982d5e93ef5a672625c4e4edb5957b8fbb7c
ANDREAS CIURILA <119446650+AndreasC55@users.noreply.github.com>
2022-12-13 18:30:41 +0000

    nothing

--- redirect.php
+++ redirect.php
@@ -1,5 +1,7 @@
+<?php 
 $firstName = $_POST['first-name'];
 $lastName = $_POST['last-name'];
 $email = $_POST['email'];
 $phoneNumber = $_POST['phone-number'];
 $relationshipStatus = $_POST['relationship-status'];
+?>
