d9b9799761f9d43a39746f65b24789391245912f
ALAN NICHOLAS <119968230+AlanNich47@users.noreply.github.com>
2022-12-15 19:53:32 +0000

    I added the form to index.php, i made it so that once you get to the redirect it sends you back to index, i connected the form to json

---
+++ redirect.php
@@ -0,0 +1,34 @@
+<?php 
+header('Location: /index.php');
+
+$firstName = $_POST['first-name'];
+$lastName = $_POST['last-name'];
+$email = $_POST['email'];
+$phoneNumber = $_POST['phone-number'];
+$relationshipStatus = $_POST['relationship-status'];
+
+// Create an array to hold the form data
+$formData = [
+    'firstName' => $firstName,
+    'lastName' => $lastName,
+    'email' => $email,
+    'phoneNumber' => $phoneNumber,
+    'relationshipStatus' => $relationshipStatus,
+];
+
+// Encode the array as a JSON string
+$formDataJson = json_encode($formData);
+
+// Save the JSON string to a file
+file_put_contents('data.json', $formDataJson);
+
+// Output a message to the user
+
+
+
+
+
+
+
+?>
+
