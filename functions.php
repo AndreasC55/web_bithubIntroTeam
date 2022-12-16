2806a83ab035d2b076f83e40f03495fd53817214
Jayden Black <119446626+jaybobo0@users.noreply.github.com>
2022-12-16 19:16:37 +0000

    Added form to the index

--- functions.php
+++ functions.php
@@ -1,9 +1,16 @@
 <?php session_start();
 
 $navigation = "
+
 <li class='nav-item'><a class='nav-link active' aria-current='page' href='/family.php'>family</a>
+
 <li class='nav-item'><a class='nav-link active' aria-current='page' href='/friends.php'>friends</a>
-<li class='nav-item'><a class='nav-link active' aria-current='page' href='/coworkers.php'>coworkers</a>";
+
+<li class='nav-item'><a class='nav-link active' aria-current='page' href='/coworkers.php'>coworkers</a>
+
+<li class='nav-item'><a class='nav-link active' aria-current='page' href='/form.php'>Contact Form</a>";
+
+
 
 $siteName = "GitHub Teams project";
 ?>
