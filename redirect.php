4812c2add439302dbd25eb74436a1a41969daaf6
Jayden Black <119446626+jaybobo0@users.noreply.github.com>
2022-12-20 20:03:07 +0000

    The new header location takes you back too the index/home page

--- redirect.php
+++ redirect.php
@@ -1,24 +1,20 @@
 <?php include $_SERVER['DOCUMENT_ROOT'].'/functions.php'; 
 
-$_SESSION["form"]["firstName"] = $_POST['first-name'];
-$_SESSION["form"]["lastName"] = $_POST['last-name'];
-$_SESSION["form"]["email"] = $_POST['email'];
-$_SESSION["form"]["phoneNumber"] = $_POST['phone-number'];
-$_SESSION["form"]["relationshipStatus"] = $_POST['relationship-status'];
+$_SESSION["form"]["fName"] = $_POST['fName'];
+
 
 $newFormData = array(
                   "date"=>date("m/d/Y"),
-                  "fName"=> $_POST['form']['first-name'], 
-                  "lName"=> $_POST['form']['last-name'],
-                  "email"=> $_POST['form']['email'],
-                  "phone-number"=> $_POST['form']['phone-number'],
-                  "relationship-status"=> $_POST['form']['relationship-status']
+                  "fName"=> $_POST['form']['fName'], 
   );
 
 
   //import json data from files
+
   $jsonFormData = file_get_contents( $_SERVER['DOCUMENT_ROOT'].'/data.json');
+
 //convert json data to php array
+
   $pastFormData = json_decode($jsonFormData,TRUE);
  
 array_push($pastFormData, $newFormData);
@@ -28,11 +24,7 @@ $jsonData = json_encode($pastFormData);
 file_put_contents( $_SERVER['DOCUMENT_ROOT'].'/data.json', $jsonData);
   
 
-header('Location: /form.php');
-
-
-
-
+header('Location: /index.php');
 
 
 ?>
