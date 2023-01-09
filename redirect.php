e2ee8950439b6edbd0281133d387cd0cc6d8b753
Jayden Black <119446626+jaybobo0@users.noreply.github.com>
2023-01-09 18:30:43 +0000

    data now displays into data.json

--- redirect.php
+++ redirect.php
@@ -1,24 +1,30 @@
 <?php include $_SERVER['DOCUMENT_ROOT'].'/functions.php'; 
 
-$_SESSION["form"]["fName"] = $_POST['fName'];
+$_SESSION["form"]["first-name"] = $_POST['first-name'];
+$_SESSION["form"]["last-name"] = $_POST['last-name'];
+$_SESSION["form"]["email"] = $_POST['email'];
+$_SESSION["form"]["phone-number"] = $_POST['phone-number'];
+$_SESSION["form"]["relationship-status"] = $_POST['relationship-status'];
 
 
 $newFormData = array(
                   "date"=>date("m/d/Y"),
-                  "fName"=> $_POST['form']['fName'], 
+                  "fName"=> $_SESSION['form']['first-name'],
+                  "lName"=> $_SESSION['form']['last-name'],
+                  "email"=> $_SESSION['form']['email'],
+                  "phone-number"=> $_SESSION['form']['phone-number'],
+                  "relationship-status"=> $_SESSION['form']['relationship-status']
   );
 
 
-  //import json data from files
+
 
   $jsonFormData = file_get_contents( $_SERVER['DOCUMENT_ROOT'].'/data.json');
-
-//convert json data to php array
 
   $pastFormData = json_decode($jsonFormData,TRUE);
  
 array_push($pastFormData, $newFormData);
-//new encode
+
 $jsonData = json_encode($pastFormData);
 
 file_put_contents( $_SERVER['DOCUMENT_ROOT'].'/data.json', $jsonData);
