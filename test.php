347d90a9df961d9cd6aaabcc8fcbd11b80d5bb46
Jayden Black <119446626+jaybobo0@users.noreply.github.com>
2023-01-10 18:34:09 +0000

    started working on the uids for the data

--- test.php
+++ test.php
@@ -1,5 +1,6 @@
 <?php
-$data = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/functions.php');
+$data = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/form.php');
+
   $data = json_decode($data, TRUE);
 
 
@@ -8,14 +9,19 @@ $data = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/functions.php');
     foreach ($data as &$item) {
       $item['uid'] = $uid;
       $uid++;
-    }
+    };
 
+$data = json_encode($data, TRUE);
 
+$fileName = $_SERVER['DOCUMENT_ROOT'].'/data.json';
 
+file_put_contents($fileName, $data);
 
+echo '<pre>';
 
+var_dump($data)
 
-
+echo '</pre>';
 
 
 
