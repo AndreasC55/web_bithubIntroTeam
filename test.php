b68d6c2cc30d68cb51cc942b9e0deb1f94de4cf5
ANDREAS CIURILA <119446650+AndreasC55@users.noreply.github.com>
2023-01-13 19:22:36 +0000

    deleted unnecessary code

--- test.php
+++ test.php
@@ -1,29 +1,4 @@
 <?php
 $data = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/form.php');
-
   $data = json_decode($data, TRUE);
-
-
-
-  $uid = 001;
-    foreach ($data as &$item) {
-      $item['uid'] = $uid;
-      $uid++;
-    };
-
-$data = json_encode($data, TRUE);
-
-$fileName = $_SERVER['DOCUMENT_ROOT'].'/data.json';
-
-file_put_contents($fileName, $data);
-
-echo '<pre>';
-
-var_dump($data)
-
-echo '</pre>';
-
-
-
-
 ?>
